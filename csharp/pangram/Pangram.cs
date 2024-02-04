f5501287741e87560a4fa02fc81fd33735e3ef08
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2024-02-04 16:55:02 +0100

    done csharp/Pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var lowercaseAsciiLetters = GetAsciiLetters();
+        foreach (var c in input)
+        {
+            lowercaseAsciiLetters.Remove(char.ToLower(c));
+
+            if (lowercaseAsciiLetters.Count == 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static List<char> GetAsciiLetters()
+    {
+        var lowercaseAsciiLetters = new List<char>();
+        for (var i = (int)'a'; i < (int)'z'; i++)
+        {
+            lowercaseAsciiLetters.Add((char)i);
+        }
+        return lowercaseAsciiLetters;
+    }
+
+}
